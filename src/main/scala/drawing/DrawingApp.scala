c917db828a060bf43f0bd467cd15b349daf724b4
Yuriy Tumakha <tumakha@gmail.com>
2019-07-08 18:45:58 +0100

    exit on Quit command

--- src/main/scala/drawing/DrawingApp.scala
+++ src/main/scala/drawing/DrawingApp.scala
@@ -10,7 +10,7 @@ object DrawingApp extends App {
 
   val screen = Screen()
 
-  private def draw(command: Command): Unit =
+  private def draw(command: Command): Command = {
     screen.draw(command) match {
       case Success(canvas) => println(canvas)
       case Failure(exception) => {
@@ -18,13 +18,14 @@ object DrawingApp extends App {
         println(screen.printCanvas) // print previous saved canvas
       }
     }
+    command
+  }
 
   private def printError(error: String): Unit = println(s"ERROR: $error")
 
   private def run: AnyVal =
     Command.parse(readLine("enter command: ")) match {
-      case Right(Quit) => true
-      case Right(command) => draw(command)
+      case Right(command) => draw(command) == Quit
       case Left(error) => printError(error)
     }
 
