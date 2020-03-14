52d40a569d4b492b48329ad22389a0111fa05428
Yuriy Tumakha <tumakha@gmail.com>
2020-03-14 23:47:09 +0000

    Drawing

--- src/main/scala/drawing/screen/Drawing.scala
+++ src/main/scala/drawing/screen/Drawing.scala
@@ -15,14 +15,7 @@ class Drawing(val initScreen: Screen) {
 
   type Canvas = Array[Array[Char]]
 
-  implicit def Canvas2Content(canvas: Canvas): Content = canvas.map(_.mkString).mkString("\n")
-
-  private lazy val canvas: Canvas =
-    if (screen.content.isEmpty) throw new IllegalStateException("Canvas is not created")
-    else screen.content.split("\n").map(_.toCharArray)
-
-  private lazy val maxX: Int = canvas(0).length - 2
-  private lazy val maxY: Int = canvas.length - 2
+  private def toContent(canvas: Canvas): Content = canvas.map(_.mkString).mkString("\n")
 
   def draw(command: Command): Try[Screen] =
     Try {
@@ -41,7 +34,7 @@ class Drawing(val initScreen: Screen) {
     DrawingScreen(command, content, screen)
   }
 
-  private def createCanvas(width: Int, height: Int): Canvas = {
+  private def createCanvas(width: Int, height: Int): Content = {
     def initialValue(i: Int, j: Int): Char =
       if (i == 0 || i == height + 1) '-'
       else if (j == 0 || j == width + 1) '|'
@@ -55,19 +48,40 @@ class Drawing(val initScreen: Screen) {
     else if (height < 0 || height > maxHeight)
       throw new IllegalArgumentException(s"Canvas height should be in range 0..$maxHeight")
 
-    Array.tabulate(height + 2, width + 2)(initialValue)
+    toContent(Array.tabulate(height + 2, width + 2)(initialValue))
   }
 
-  private def drawLine(line: Line): Canvas = drawingByJoinPoints(line.point1, line.point2)
+  private def drawLine(line: Line): Content = drawingByJoinPoints(line.point1, line.point2)
 
-  private def drawRectangle(rec: Rectangle): Canvas = {
+  private def drawRectangle(rec: Rectangle): Content = {
     val point21 = Point(rec.point2.x, rec.point1.y)
     val point12 = Point(rec.point1.x, rec.point2.y)
 
     drawingByJoinPoints(rec.point1, point21, rec.point2, point12, rec.point1)
   }
 
-  private def drawingByJoinPoints(point: Point*): Canvas = {
+  private def drawingByJoinPoints(point: Point*): Content = {
+    val canvas: Canvas =
+        if (screen.content.isEmpty) throw new IllegalStateException("Canvas is not created")
+        else screen.content.split("\n").map(_.toCharArray)
+
+    val maxX: Int = canvas(0).length - 2
+    val maxY: Int = canvas.length - 2
+
+    def safeX(x: Int): Int = safeInt(x, maxX)
+
+    def safeY(y: Int): Int = safeInt(y, maxY)
+
+    def xInsideCanvas(x: Int): Boolean = insideCanvas(x, maxX)
+
+    def yInsideCanvas(x: Int): Boolean = insideCanvas(x, maxY)
+
+    def checkAnyPointInsideCanvas(point: Point*): Unit = {
+      val points = point.distinct
+      points.find(p => xInsideCanvas(p.x) && yInsideCanvas(p.y))
+        .orElse(throw new IllegalArgumentException(s"All points are outside canvas area. ${points.toList.sortBy(_.x)}"))
+    }
+
     def drawSingleLine(point1: Point, point2: Point): Point = {
       for {
         x <- rangeAsc(point1.x, point2.x)
@@ -78,27 +92,13 @@ class Drawing(val initScreen: Screen) {
 
     checkAnyPointInsideCanvas(point: _*)
     point.reduce(drawSingleLine)
-    canvas
+    toContent(canvas)
   }
-
-  private def safeX(x: Int): Int = safeInt(x, maxX)
-
-  private def safeY(y: Int): Int = safeInt(y, maxY)
 
   private def safeInt(n: Int, max: Int): Int =
     if (n < 1) 1
     else if (n > max) max
     else n
-
-  private def checkAnyPointInsideCanvas(point: Point*): Unit = {
-    val points = point.distinct
-    points.find(p => xInsideCanvas(p.x) && yInsideCanvas(p.y))
-      .orElse(throw new IllegalArgumentException(s"All points are outside canvas area. ${points.toList.sortBy(_.x)}"))
-  }
-
-  private def xInsideCanvas(x: Int): Boolean = insideCanvas(x, maxX)
-
-  private def yInsideCanvas(x: Int): Boolean = insideCanvas(x, maxY)
 
   private def insideCanvas(value: Int, max: Int): Boolean = value > 0 && value <= max
 
